3e60338b887ab11505ce35c4b9c49236e5deb6a8
Yuri Goncharuk <yukon39@gmail.com>
2022-04-24 12:58:02 +0300

    feat: Add core IISServerManager object

---
+++ src/IISAdministration/IISServerManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Web.Administration;
+using ScriptEngine.Machine.Contexts;
+
+namespace com.github.yukon39.IISAdministration
+{
+    [ContextClass(typeName: "IISServerManager", typeAlias: "МенеджерСервераIIS")]
+    public class IISServerManager : AutoContext<IISServerManager>, IObjectWrapper
+    {
+        private readonly ServerManager serverManager;
+
+        [ScriptConstructor]
+        public static IISServerManager ScriptConstructor()
+            => new IISServerManager();
+
+        private IISServerManager()
+            => serverManager = new ServerManager();
+
+        [ContextMethod("CommitChanges", "ЗаписатьИзменения")]
+        public void CommitChanges()
+            => serverManager.CommitChanges();
+
+        public object UnderlyingObject 
+            => serverManager;
+    }
+}

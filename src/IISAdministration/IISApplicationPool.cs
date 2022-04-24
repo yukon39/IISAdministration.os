7269ccf9779b718f16ce9409dc220b74875a6a5b
Yuri Goncharuk <yukon39@gmail.com>
2022-04-24 13:46:49 +0300

    feat: Add application pools wrappers

---
+++ src/IISAdministration/IISApplicationPool.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Web.Administration;
+using ScriptEngine.Machine.Contexts;
+
+namespace com.github.yukon39.IISAdministration
+{
+    [ContextClass(typeName: "IISApplicationPool", typeAlias: "ПулПриложенийIIS")]
+    public class IISApplicationPool : AutoContext<IISApplicationPool>, IObjectWrapper
+    {
+        private readonly ApplicationPool applicationPool;
+
+        public IISApplicationPool(ApplicationPool applicationPool)
+            => this.applicationPool = applicationPool;
+
+        public object UnderlyingObject
+            => applicationPool;
+
+        public override string AsString()
+            => applicationPool.Name;
+
+        [ContextProperty("Name", "Имя")]
+        public string Name
+            => applicationPool.Name;
+
+        [ContextMethod("Rename", "Переименовать")]
+        public string Rename(string newName)
+            => applicationPool.Name = newName;
+
+        [ContextProperty("Enable32BitAppOnWin64", "Разрешить32БитныеПриложения")]
+        public bool Enable32BitAppOnWin64
+        {
+            get => applicationPool.Enable32BitAppOnWin64;
+            set => applicationPool.Enable32BitAppOnWin64 = value;
+        }
+
+        [ContextProperty("ManagedPipelineMode", "Разрешить32БитныеПриложения")]
+        public string ManagedPipelineMode
+            => applicationPool.ManagedPipelineMode.ToString();
+
+        [ContextProperty("ManagedRuntimeVersion", "Разрешить32БитныеПриложения")]
+        public string ManagedRuntimeVersion
+            => applicationPool.ManagedRuntimeVersion;
+
+        [ContextProperty("State", "Состояние")]
+        public string State
+            => applicationPool.State.ToString();
+
+        [ContextMethod("Start", "Запустить")]
+        public void Start()
+            => applicationPool.Start();
+
+        [ContextMethod("Stop", "Остановить")]
+        public void Stop()
+            => applicationPool.Stop();
+
+        [ContextMethod("Recycle", "Перезапустить")]
+        public void Recycle()
+            => applicationPool.Recycle();
+    }
+}
---
+++ src/IISAdministration/IISApplicationPoolCollection.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Web.Administration;
+using ScriptEngine.Machine.Contexts;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace com.github.yukon39.IISAdministration
+{
+    [ContextClass(typeName: "IISApplicationPoolCollection", typeAlias: "КоллекцияПуловПриложенийIIS")]
+    public class IISApplicationPoolCollection : AutoContext<IISApplicationPoolCollection>, ICollectionContext, IEnumerable<IISApplicationPool>, IObjectWrapper
+    {
+        private readonly ApplicationPoolCollection applicationPools;
+        private readonly List<IISApplicationPool> collection = new List<IISApplicationPool>();
+
+        public IISApplicationPoolCollection(ApplicationPoolCollection applicationPools)
+        {
+            this.applicationPools = applicationPools;
+            applicationPools.ToList().ForEach(
+                x => collection.Add(new IISApplicationPool(x))
+            );
+        }
+
+        public object UnderlyingObject
+            => applicationPools;
+
+        [ContextMethod("Count", "Количество")]
+        public int Count()
+            => applicationPools.Count;
+
+        [ContextMethod("Add", "Добавить")]
+        public IISApplicationPool Add(string name)
+        {
+            var appPool = applicationPools.Add(name);
+            var item = new IISApplicationPool(appPool);
+            collection.Add(item);
+            return item;
+        }
+
+        [ContextMethod("Remove", "Удалить")]
+        public void Remove(IISApplicationPool item)
+        {
+            var appPool = (ApplicationPool)item.UnderlyingObject;
+            applicationPools.Remove(appPool);
+            collection.Remove(item);
+        }
+
+        public IEnumerator<IISApplicationPool> GetEnumerator()
+            => collection.GetEnumerator();
+
+        public CollectionEnumerator GetManagedIterator()
+            => new CollectionEnumerator(GetEnumerator());
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
